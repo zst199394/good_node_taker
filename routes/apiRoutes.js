55dab6a2c2241c1e034767a9d45463fdcfdb5e22
shuting <zst9394@outlook.com>
2021-03-08 23:44:02 -0500

    change apiRoute

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,14 @@
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
+const fs = require('fs');
 const path = require('path');
 const note = {};
+// const util = require('util');
+// const fs = require('fs');
+// // const uuid = require('uuid/v1');
+// // const readFile = util.promisify(fs.readFile);
+// // const writeFile = util.promisify(fs.writeFile);
 
 // ROUTING
 
@@ -13,15 +19,20 @@ module.exports = (app) => {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get('/api/notes', (req, res) => res.json(note));
+  app.get('/api/notes', (req, res) => res.json(data));
 
   app.post('/api/notes', (req, res) => {
     
       data.push(req.body);
-      res.json(true);
+      
+      // fs.writeFile('../data/db.json', JSON.stringify(data), "utf-8", (err) => {
+      //   err? console.error(err) : console.log("Success to save ur note!!!!!!!!!");
+      //   res.json(req.body);
+      // })
+
    
   });
-
+ 
 
   app.post('/api/clear', (req, res) => {
     data.length = 0;
