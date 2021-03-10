1c4d533f97bcfb8a8abb4587183758fc837e343f
shuting <zst9394@outlook.com>
2021-03-09 19:00:17 -0500

    update apiRoute

--- data/data.js
+++ data/data.js
@@ -15,21 +15,3 @@
 
 //     }
 //     addNotes(text){}
-
-
-
-// }
-// DATA
-// Below data will hold all of the waitlist tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-
-// [
-//   {
-//       "title":"Test Title",
-//       "text":"Test text"
-//   }
-// ]
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  // module.exports = noteArray;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,38 +1,31 @@
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+
 const fs = require('fs');
 const path = require('path');
-const note = {};
+const notes = require('../data/db.json');
+const { v4: uuidv4 } = require('uuid');
+// const data = [];
 // const util = require('util');
 // const fs = require('fs');
-// // const uuid = require('uuid/v1');
+// const uuid = require('uuid/v1');
 // // const readFile = util.promisify(fs.readFile);
 // // const writeFile = util.promisify(fs.writeFile);
 
 // ROUTING
 
 module.exports = (app) => {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
-  app.get('/api/notes', (req, res) => res.json(data));
+  app.get('/api/notes', (req, res) => res.json(notes));
 
   app.post('/api/notes', (req, res) => {
-    
-      data.push(req.body);
-      
-      // fs.writeFile('../data/db.json', JSON.stringify(data), "utf-8", (err) => {
-      //   err? console.error(err) : console.log("Success to save ur note!!!!!!!!!");
-      //   res.json(req.body);
-      // })
-
-   
+    req.body.id = uuidv4();
+    data.push(req.body);
+    console.log(req.body);
+    fs.writeFile('../data/db.json', JSON.stringify(data), "utf-8", (err) => {
+      err ? console.error(err) : console.log("Success to save ur note!!!!!!!!!");
+      res.json(req.body);
+    })
   });
- 
+
 
   app.post('/api/clear', (req, res) => {
     data.length = 0;
